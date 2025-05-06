1f299047e9fe635e4a03589221ec76f6cad6d8f4
Nelo-T. Wallus <n.wallus@sap.com>
2025-05-06 15:07:51 +0200

    Add WhoamiStatus.URL
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- api/v1alpha1/whoami_types.go
+++ api/v1alpha1/whoami_types.go
@@ -20,22 +20,15 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // WhoamiSpec defines the desired state of Whoami.
-type WhoamiSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Whoami. Edit whoami_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
-}
+type WhoamiSpec struct{}
 
 // WhoamiStatus defines the observed state of Whoami.
 type WhoamiStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:format:URL
+	// +required
+	URL string `json:"url"`
 }
 
 // +kubebuilder:object:root=true
