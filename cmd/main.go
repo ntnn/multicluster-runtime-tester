bea52478e5cc65eec68e4124e89b048cafa04770
Nelo-T. Wallus <n.wallus@sap.com>
2025-05-06 15:40:46 +0200

    kubebuilder create api --group kube-bind --version v1alpha1 --kind APIResourceSchema
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- cmd/main.go
+++ cmd/main.go
@@ -219,6 +219,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "APIServiceExport")
 		os.Exit(1)
 	}
+	if err = (&kubebindcontroller.APIResourceSchemaReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "APIResourceSchema")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
