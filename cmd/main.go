c08160ae35c6f5381ee202a331c268736548feb4
Nelo-T. Wallus <n.wallus@sap.com>
2025-05-06 15:50:53 +0200

    kubebuilder create api --group kube-bind --version v1alpha1 --kind APIServiceBinding
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- cmd/main.go
+++ cmd/main.go
@@ -240,6 +240,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "APIServiceExportRequest")
 		os.Exit(1)
 	}
+	if err = (&kubebindcontroller.APIServiceBindingReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "APIServiceBinding")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
