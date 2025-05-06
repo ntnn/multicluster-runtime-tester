4e2bdfabb14da0ec57c1f9528829a626c62c54c5
Nelo-T. Wallus <n.wallus@sap.com>
2025-05-06 15:49:59 +0200

    APIServiceExportRequest
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- api/kube-bind/v1alpha1/apiserviceexportrequest_types.go
+++ api/kube-bind/v1alpha1/apiserviceexportrequest_types.go
@@ -20,16 +20,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // APIServiceExportRequestSpec defines the desired state of APIServiceExportRequest.
 type APIServiceExportRequestSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of APIServiceExportRequest. Edit apiserviceexportrequest_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Resources        []ResourceRef     `json:"resources,omitempty"`
+	PermissionClaims []PermissionClaim `json:"permissionClaims"`
 }
 
 // APIServiceExportRequestStatus defines the observed state of APIServiceExportRequest.
