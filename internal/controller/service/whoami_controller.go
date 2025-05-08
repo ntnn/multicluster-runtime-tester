4b31e41918317f99702cc6d0a81a2499bec1328e
Nelo-T. Wallus <n.wallus@sap.com>
2025-05-08 12:31:04 +0200

    Implement whoami reconciler
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- internal/controller/service/whoami_controller.go
+++ internal/controller/service/whoami_controller.go
@@ -18,7 +18,14 @@ package service
 
 import (
 	"context"
+	"errors"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	mctrl "sigs.k8s.io/multicluster-runtime"
@@ -45,11 +52,130 @@ type WhoamiReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.20.4/pkg/reconcile
 func (r *WhoamiReconciler) Reconcile(ctx context.Context, req mctrl.Request) (mctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	log := logf.FromContext(ctx).WithValues("cluster", req.ClusterName, "namespace", req.Namespace, "name", req.Name)
+	log.Info("Reconciling Whoami")
 
-	// TODO(user): your logic here
+	cluster, err := r.Manager.GetCluster(ctx, req.ClusterName)
+	if err != nil {
+		return mctrl.Result{}, err
+	}
+
+	cl := cluster.GetClient()
+
+	whoami := &servicev1alpha1.Whoami{}
+	if err := cl.Get(ctx, req.NamespacedName, whoami); err != nil {
+		if apierrors.IsNotFound(err) {
+			return mctrl.Result{}, DeleteWhoami(ctx, cl, req)
+		}
+		return mctrl.Result{}, err
+	}
+
+	if err := EnsureWhoami(ctx, cl, req); err != nil {
+		log.Error(err, "Failed to ensure Whoami resources")
+		return mctrl.Result{}, err
+	}
 
 	return mctrl.Result{}, nil
+}
+
+var usedResourceTypes = []client.Object{
+	&appsv1.Deployment{},
+	&corev1.Service{},
+}
+
+func GenWhoamiResources(req mctrl.Request) []client.Object {
+	meta := metav1.ObjectMeta{
+		Name:      req.Name,
+		Namespace: req.Namespace,
+		Labels: map[string]string{
+			"whoami": req.Name,
+		},
+	}
+
+	return []client.Object{
+		&appsv1.Deployment{
+			ObjectMeta: meta,
+			Spec: appsv1.DeploymentSpec{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"whoami": req.Name,
+					},
+				},
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: req.Namespace,
+						Labels: map[string]string{
+							"whoami": req.Name,
+						},
+					},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:  "whoami",
+								Image: "traefik/whoami",
+							},
+						},
+					},
+				},
+			},
+		},
+		&corev1.Service{
+			ObjectMeta: meta,
+			Spec: corev1.ServiceSpec{
+				Selector: map[string]string{
+					"whoami": req.Name,
+				},
+				Ports: []corev1.ServicePort{
+					{
+						Port:       80,
+						TargetPort: intstr.FromInt(80),
+					},
+				},
+			},
+		},
+	}
+}
+
+func EnsureWhoami(ctx context.Context, cl client.Client, req mctrl.Request) error {
+	log := logf.FromContext(ctx)
+	log.Info("Creating Whoami")
+
+	var errs error
+	for _, resource := range GenWhoamiResources(req) {
+		log.Info("Creating Whoami resource", "resource", resource)
+
+		if err := cl.Get(ctx, client.ObjectKeyFromObject(resource), resource); err != nil {
+			// Resource doesn't exist, create it
+			if err := cl.Create(ctx, resource); err != nil {
+				log.Error(err, "Failed to create Whoami resource", "resource", resource)
+				err = errors.Join(errs, err)
+			}
+		} else {
+			// Resource exists, update it
+			if err := cl.Update(ctx, resource); err != nil {
+				log.Error(err, "Failed to update Whoami resource", "resource", resource)
+				err = errors.Join(errs, err)
+			}
+		}
+	}
+	return errs
+}
+
+func DeleteWhoami(ctx context.Context, cl client.Client, req mctrl.Request) error {
+	log := logf.FromContext(ctx)
+	log.Info("Deleting Whoami")
+
+	var errs error
+	for _, resource := range usedResourceTypes {
+		if err := cl.DeleteAllOf(ctx, resource,
+			client.InNamespace(req.Namespace),
+			client.MatchingLabels{"whoami": req.Name},
+		); err != nil {
+			log.Error(err, "Failed to delete Whoami resources", "resource", resource)
+			err = errors.Join(errs, err)
+		}
+	}
+	return errs
 }
 
 // SetupWithManager sets up the controller with the Manager.
